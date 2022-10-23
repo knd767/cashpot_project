51bec4ed476db30f40316e585e169ccfc6e1e83f
knd767 <108214839+knd767@users.noreply.github.com>
2022-10-22 23:01:59 -0400

    File Changes
    The following changes were made
    1) Each table was saved as a CSV to be exported to Tableau
    2) A data frame called win_loss_percent was created to calculated to summarize each draw year

--- Cashpot_project.R
+++ Cashpot_project.R
@@ -16,6 +16,8 @@ winner_trend<- nlcb %>%
   group_by(draw_year)%>%
   summarize(yearly_wins = sum(num_of_wins), avg_wins = mean(num_of_wins),
             median_wins = median(num_of_wins))
+
+write.csv(winner_trend,"winner_trend.csv")
   
 
 #Plots Winner data to view trend of total wins per year 
@@ -48,12 +50,16 @@ jackpot_sum <- nlcb %>%
   summarize( yearly_avg_jackpot = mean(jackpot),
              yearly_mid_jackpot = median(jackpot)) 
 
+write.csv(jackpot_sum,"jacpot_summary.csv")
+
 #Summarizes Jackpot per Winner
 jackpot_per_winner_sum <- nlcb %>%
   filter(draw_year >= 2000)%>%
   filter(num_of_wins >= 1 )%>%
   group_by(draw_year) %>%
   summarize(avg_jackpot_per_winner= mean(jackpot_per_winner))
+
+write.csv(jackpot_per_winner_sum,"jackpot_per_winner_summary.csv")
 
 #Boxplot used to quickly test for outliers
 ggplot(jackpot_per_winner_sum, aes(avg_jackpot_per_winner,color= draw_year,y = 1,))+
@@ -74,15 +80,40 @@ jackpot_outlier <- nlcb %>%
   filter( jackpot > jackpot_upperlimit| jackpot < jackpot_lowerlimit)
   
 
-#Number of Losses per year
+#Number of Losses per year. This counts each draw that did not have a winner
 yearly_loss <- nlcb %>%
   filter(num_of_wins == 0)%>%
-  count(draw_year, sort = TRUE)
+  filter(draw_year >= 2000)%>%
+  group_by(draw_year)%>%
+  count(draw_year, sort = TRUE)%>%
+  rename(num_of_draw_losses = n)
+
+#Counts the draws that had a winner and summarizes this data on a yearly scale. 
+winner_draw_count <- nlcb %>%
+  filter(num_of_wins >= 1)%>%
+  filter(draw_year >= 2000)%>%
+  group_by(draw_year)%>%
+  count(draw_year)%>%
+  rename(num_of_draw_wins = n)
+
 
 #Calculate how much draws happened per year
 yearly_draw_amt <- nlcb %>%
   arrange(draw_year)%>%
   count(draw_year, sort = FALSE)
+
+#Joins Losses to  Wins and calculates a percentage
+
+win_loss_percent<- winner_draw_count%>%
+  left_join(yearly_loss, "draw_year")%>%
+  replace_na(list(num_of_draw_losses = 0))%>%
+  transmute(draw_year,num_of_draw_wins, num_of_draw_losses, 
+            #Calculates total draws
+            total_draws = num_of_draw_wins + num_of_draw_losses, 
+            win_percent = num_of_draw_wins / total_draws * 100)
+
+write.csv(win_loss_percent, "win_loss_percent.csv")
+
 
 
  
